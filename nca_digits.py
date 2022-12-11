f68bf76fe7bb927ba2346f8536b8e9e9b4670baf
Manan Mehta <57878537+mananm2@users.noreply.github.com>
2022-12-10 22:37:01 -0600

    Add files via upload
    Adding nca implementation that works with the digits dataset

--- nca_digits.py
+++ nca_digits.py
@@ -3,25 +3,10 @@ import torchvision
 import torch.nn as nn
 import numpy as np
 import tqdm
-from datasets import *
-import eval_dataset
-from evaluate import *
-from torchvision import transforms
+import os
 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
-
-def squared_euclidean_distance(vec):
-    """
-    Get the squared pairwise Euclidean distance matrix for vec
-
-    """
-    dot = torch.mm(vec, torch.t(vec))
-    norm_sq = torch.diag(dot)
-    d = norm_sq[None, :] - 2*dot + norm_sq[:, None]
-    d = torch.clamp(d, min=0.0)  # replace negative values with 0
-    return d.float()
-
 
 def computeLoss(A, X, y_mask):
     """
@@ -52,48 +37,58 @@ def computeLoss(A, X, y_mask):
     #print(loss.item())
     return loss
 
-#datasetName = {
-#    "cub":CUBDataset,
-#    "sop":SOPDataset,
-#    "car":CARDataset
-#}["cub"]    # Change the "cub" on this line to select the correct dataset
 
-#fileToSaveA = "Avalue.pickle"
+def squared_euclidean_distance(vec):
+    """
+    Get the spairwise squared Euclidean distance matrix
 
-#dataset = datasetName()
-#params = {"batch_size" : 10, "num_workers" : 8, "shuffle":True}
-#loader = torch.utils.data.DataLoader(dataset, **params)
-#h, w = dataset.getOutputShape()
+    """
+    dot = torch.mm(vec, torch.t(vec))
+    norm_sq = torch.diag(dot)
+    d = norm_sq[None, :] - 2*dot + norm_sq[:, None]
+    d = torch.clamp(d, min = 0.0)  # replace negative values with 0
+    return d.float()
 
-batch_size = 128
-latentDims = 512
+
+data_path = './uspsdata/' # digit dataset from the paper
+if not os.path.exists(data_path):
+    os.makedirs(data_path)
+
+
+batch_size = 64
+latentDims = 32
 nEpochs = 100
 
-#train_data = eval_dataset.load(name = "cars",  root = './data/CARS/', mode = 'train', 
-#                            transform = eval_dataset.utils.make_transform())
-train_data = eval_dataset.load(name = "cub",  root = './data/CUB/', mode = 'train', 
-                            transform = eval_dataset.utils.make_transform())
-                                                            
-test_data = eval_dataset.load(name = "cub",  root = './data/CUB/', mode = 'eval', 
-                            transform = eval_dataset.utils.make_transform())
-                                                            
+train_data = torchvision.datasets.USPS('./uspsdata/', train=True, download=True,
+                             transform=torchvision.transforms.Compose([
+                               torchvision.transforms.ToTensor(),
+                               torchvision.transforms.Normalize(
+                                 (0.1307,), (0.3081,))
+                             ]))
 
-train_loader = torch.utils.data.DataLoader(train_data, batch_size = batch_size, shuffle = True)
-test_loader = torch.utils.data.DataLoader(test_data, batch_size = batch_size, shuffle = True)
+test_data = torchvision.datasets.USPS('./uspsdata/', train=False, download=True,
+                             transform=torchvision.transforms.Compose([
+                               torchvision.transforms.ToTensor(),
+                               torchvision.transforms.Normalize(
+                                 (0.1307,), (0.3081,))
+                             ]))
+
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=True)
 
 dev = "cuda" if torch.cuda.is_available() else "cpu"
 
 h, w = next(iter(train_loader))[0].shape[-2:]
 
-#X_train = np.ndarray(train_data.I).reshape((-1, h*w*3))
-#y_train = train_data.ys
-#X_test = np.ndarray(test_data.I).reshape((-1, h*w*3))
-#y_test = test_data.ys
+X_train = train_data.data.reshape(-1, h*w)
+y_train = train_data.targets
+X_test = test_data.data.reshape(-1, h*w)
+y_test = test_data.targets
 
-a = torch.randn(latentDims, h*w*3) * 1e-04 #latent A
+a = torch.randn(h*w, h*w) * 0.01  #full A
 A = nn.Parameter(a)
 
-optim = torch.optim.Adam([A], lr=1e-05)
+optim = torch.optim.Adam([A], lr=1e-04)
 
 for epoch in range(nEpochs):
     running_loss = 0
@@ -102,29 +97,25 @@ for epoch in range(nEpochs):
         
         optim.zero_grad()
         
-        X = images.reshape((-1, h*w*3))
-        
+        X = images.reshape((-1, h*w))
         y_mask = labels[:, None] == labels[None, :] # pairwise boolean class matrix
 
         loss = computeLoss(A, X, y_mask)
 
         loss.backward()
 
-        torch.nn.utils.clip_grad_norm_([A], 1e-04)
-
         optim.step()
 
         running_loss += loss.item()
-        print(loss.item())
 
     print(f"Epoch {epoch+1}/{nEpochs}: {running_loss * batch_size / len(train_data) :.4f}")
 
-"""
+# Find the train and test accuracies
 A = A.detach().cpu().numpy()
 
 X_train_embed = X_train @ A.T
 X_test_embed = X_test @ A.T
-knn = KNeighborsClassifier(n_neighbors=5)
+knn = KNeighborsClassifier(n_neighbors = 5)
 knn.fit(X_train_embed, y_train)
 train_pred = knn.predict(X_train_embed)
 test_pred = knn.predict(X_test_embed)
@@ -134,6 +125,5 @@ test_acc = accuracy_score(test_pred, y_test)
 
 print(f"Train Accuracy: {train_acc:.4f}")
 print(f"Test Accuracy: {test_acc:.4f}")
-"""
 
-print("")
+
